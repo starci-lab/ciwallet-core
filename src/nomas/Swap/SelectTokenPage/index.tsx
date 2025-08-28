e7d22ef7b52143b7260f07d6ad494d7bcedeee21
starci183 <cuongnvtse160875@gmail.com>
2025-08-28 21:18:49 +0700

    1313

--- src/nomas/Swap/SelectTokenPage/index.tsx
+++ src/nomas/Swap/SelectTokenPage/index.tsx
@@ -4,22 +4,31 @@ import {
     NomasCard,
     NomasCardBody,
     NomasCardHeader,
+    TokenCard,
 } from "../../components"
 import {
     setSwapPage,
     SwapPageState,
     useAppDispatch,
     useAppSelector,
-    setTokenInChainId,
+    setTokenIn,
+    setTokenOut,
     setTokenOutChainId,
+    setTokenInChainId,
 } from "../../redux"
+import { ChainId } from "@ciwallet-sdk/types"
+import { Spacer } from "@heroui/react"
 
 export const SelectTokenPage = () => {
     const dispatch = useAppDispatch()
     const chainManager = useAppSelector((state) => state.chain.manager)
-    const tokenInChainId = useAppSelector((state) => state.swap.tokenInChainId)
-    const tokenOutChainId = useAppSelector((state) => state.swap.tokenOutChainId)
+    const tokenInChainId =
+    useAppSelector((state) => state.swap.tokenInChainId) ?? ChainId.Monad
+    const tokenOutChainId =
+    useAppSelector((state) => state.swap.tokenOutChainId) ?? ChainId.Monad
+    const tokenManager = useAppSelector((state) => state.token.manager)
     const isInput = useAppSelector((state) => state.swap.isInput)
+    const network = useAppSelector((state) => state.base.network)
     return (
         <>
             <NomasCardHeader
@@ -55,14 +64,15 @@ export const SelectTokenPage = () => {
                                     }
                                     return (
                                         <NomasAvatar
-                                            as="button"
-                                            onClick={() => {
-                                                if (isInput) {
-                                                    dispatch(setTokenInChainId(chain.id))
-                                                } else {
-                                                    dispatch(setTokenOutChainId(chain.id))
+                                            onClick={
+                                                () => {
+                                                    if (isInput) {
+                                                        dispatch(setTokenInChainId(chain.id))
+                                                    } else {
+                                                        dispatch(setTokenOutChainId(chain.id))
+                                                    }
                                                 }
-                                            }}
+                                            }   
                                             key={chain.id}
                                             dimension="origin"
                                             src={chain.iconUrl}
@@ -72,6 +82,33 @@ export const SelectTokenPage = () => {
                                 })()
                             )}
                         </div>
+                    </NomasCardBody>
+                </NomasCard>
+                <Spacer y={4}/>
+                <NomasCard className="bg-content3">
+                    <NomasCardBody className="gap-2">
+                        {tokenManager
+                            .getTokensByChainIdAndNetwork(tokenInChainId, network)
+                            .map((token) => {
+                                return (
+                                    <TokenCard
+                                        isPressable={true}
+                                        onPress={
+                                            () => {
+                                                if (isInput) {
+                                                    dispatch(setTokenIn(token))
+                                                } else {
+                                                    dispatch(setTokenOut(token))
+                                                }
+                                                dispatch(setSwapPage(SwapPageState.Swap))
+                                            }
+                                        }
+                                        key={token.tokenId}
+                                        token={token}
+                                        chainId={tokenInChainId}
+                                    />
+                                )
+                            })}
                     </NomasCardBody>
                 </NomasCard>
             </NomasCardBody>
--- src/nomas/components/reusable/TokenCard/index.tsx
+++ src/nomas/components/reusable/TokenCard/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { NomasAvatar } from "../../extends"
+import { NomasAvatar, NomasCard, NomasCardBody } from "../../extends"
 import { ChainId, type Token } from "@ciwallet-sdk/types"
 import { useBalance } from "@ciwallet-sdk/hooks"
 import useSWR from "swr"
@@ -9,15 +9,16 @@ export interface TokenCardProps {
     token: Token
     chainId: ChainId
     onPress?: () => void
+    isPressable?: boolean
 }
 
-export const TokenCard = ({ token, chainId, onPress }: TokenCardProps) => {
+export const TokenCard = ({ token, chainId, onPress, isPressable = true }: TokenCardProps) => {
     const { handle } = useBalance()
     const network = useAppSelector(state => state.base.network)
     const { data } = useSWR(
         ["balance", token.address, network, token.address],
-        () => {
-            return handle({
+        async () => {
+            return await handle({
                 chainId,
                 network,    
                 address: "0xA7C1d79C7848c019bCb669f1649459bE9d076DA3",
@@ -26,25 +27,34 @@ export const TokenCard = ({ token, chainId, onPress }: TokenCardProps) => {
         }
     )
     return (
-        <div className="flex justify-between items-center" onClick={onPress}>
-            <div className="flex gap-2 items-center">
-                <div className="relative">
-                    <NomasAvatar src={token.iconUrl} />
-                    <NomasAvatar
-                        src={token.iconUrl}
-                        className="absolute bottom-0 right-0 z-50"
-                        dimension="shrink"
-                    />
+        <NomasCard
+            isPressable={isPressable}
+            className="flex items-center bg-content2"
+            onClick={onPress}
+        >
+            <NomasCardBody className="flex w-full flex-row items-center justify-between gap-2">
+                {/* Left: token info */}
+                <div className="flex flex-row items-center gap-2">
+                    <div className="relative">
+                        <NomasAvatar src={token.iconUrl} />
+                        <NomasAvatar
+                            src={token.iconUrl}
+                            className="absolute bottom-0 right-0 z-50"
+                            dimension="shrink"
+                        />
+                    </div>
+                    <div className="flex flex-col">
+                        <div className="text-sm font-medium">{token.name}</div>
+                        <div className="text-xs text-foreground-500">{token.symbol}</div>
+                    </div>
                 </div>
-                <div className="flex flex-col">
-                    <div className="text-sm">{token.name}</div>
-                    <div className="text-xs text-foreground-500">{token.symbol}</div>
+        
+                {/* Right: balance */}
+                <div className="flex flex-col text-right">
+                    <div className="text-sm font-medium">{data ?? "--"}</div>
+                    <div className="text-xs text-foreground-500">0</div>
                 </div>
-            </div>
-            <div>
-                <div className="text-sm">{data}</div>
-                <div className="text-xs text-foreground-500">0</div>
-            </div>
-        </div>
+            </NomasCardBody>
+        </NomasCard>
     )
 }
